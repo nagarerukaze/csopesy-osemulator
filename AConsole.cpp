7b8fa4aecc16f79bfe96ca210c00b82478b50a57
Nicole Jocson <nicpjocson@gmail.com>
2024-09-25 23:21:36 +0800

    Fix accidentally put definitions in AConsole.cpp

--- AConsole.cpp
+++ AConsole.cpp
@@ -1,18 +1,1 @@
-#pragma once
-#include <string>
-
-class AConsole
-{
-public:
-	typedef std::string String;
-	AConsole(String name);
-	~AConsole() = default;
-
-	String getName();
-	virtual void onEnabled() = 0;
-	virtual void display() = 0;
-	virtual void process() = 0;
-
-	String name;
-	friend class ConsoleManager;
-};
+#include "AConsole.h"
