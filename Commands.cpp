d71f91aca6a0543c129bd592c7e27c43b8b20f3f
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-09-27 01:28:54 +0800

    Updated Commands files

---
+++ Commands.cpp
@@ -0,0 +1,29 @@
+#include "Commands.h"
+
+void Commands::printHeader() {
+
+}
+
+void Commands::initialize() {
+
+}
+
+void Commands::screen() {
+
+}
+
+void Commands::schedulerTest() {
+  
+}
+
+void Commands::schedulerStop() {
+    
+}
+
+void Commands::reportUtil() {
+    
+}
+
+void Commands::clear() {
+    
+}
---
+++ Commands.h
@@ -0,0 +1,14 @@
+#pragma once
+class Commands
+{
+public:
+	void printHeader();
+    void printHeader();
+    void initialize();
+    void screen();
+    void schedulerTest();
+    void schedulerStop();
+    void reportUtil();
+    void clear();
+};
+
