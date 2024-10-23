1ce8220c78638637324cdbb42c7131eacc40c772
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-10-24 05:15:33 +0800

    Updated "Last updated"

--- ConsoleManager.cpp
+++ ConsoleManager.cpp
@@ -49,7 +49,7 @@ void ConsoleManager::printHeader() {
     std::cout << "Jocson, Nicole Pedernal (S11)" << std::endl;
     std::cout << "Rebano, Jaeme Patrice (S11)" << std::endl;
     std::cout << "Jaramillo, Brandon Anthony (S12)" << std::endl << std::endl;
-    std::cout << "Last updated: 10-20-2024" << std::endl; // TODO: Update the Date
+    std::cout << "Last updated: 10-24-2024" << std::endl; // TODO: Update the Date
     std::cout << "---------------------------------------------------------------" << std::endl;
     std::cout << std::endl;
 }
